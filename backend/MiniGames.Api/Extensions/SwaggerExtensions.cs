4338699fb3365ce67a7891c42ab053927d7bf979
Gicu Caraman <caramangicu25@gmail.com>
2026-03-17 09:30:00 +0200

    Add Swagger/OpenAPI extension methods

---
+++ backend/MiniGames.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,34 @@
+using Microsoft.OpenApi.Models;
+
+namespace MiniGames.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddMiniGamesSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "MiniGames API", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "Enter your JWT token.",
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+        return services;
+    }
+}
