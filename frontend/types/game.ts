b0947d6c446cf242a0a03181de7915757e7d4110
Gicu Caraman <caramangicu25@gmail.com>
2026-04-04 10:30:00 +0200

    Add Game type definitions

---
+++ frontend/types/game.ts
@@ -0,0 +1,15 @@
+export type GameId =
+  | "sudoku"
+  | "memory"
+  | "click-speed"
+  | "snake"
+  | "tic-tac-toe";
+
+export interface GameMeta {
+  id: GameId;
+  title: string;
+  description: string;
+  href: string;
+  icon: string;
+  color: string;
+}
